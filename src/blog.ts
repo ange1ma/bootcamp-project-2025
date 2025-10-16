760310d96a7db60e9071f728a84d91045d300d8d
ange1ma <siyinmaa@gmail.com>
2025-10-16 02:00:41 -0700

    feat: create an array of Blogs

--- src/blog.ts
+++ src/blog.ts
@@ -6,3 +6,22 @@ type Blog = {
     imageAlt: string;
     slug: string;
 };
+
+const blogs: Blog[] = [
+    {
+        title: "hello world!",
+        date: "10-10-2025",
+        description: "first blog ever",
+        image: "file path for image",
+        imageAlt: "this is an image",
+        slug: "hello-world",
+    },
+    {
+        title: "grwm for halloween",
+        date: "10-15-2025",
+        description: "planning costume ideas",
+        image: "file path for image",
+        imageAlt: "this is an image",
+        slug: "grwm-for-halloween",
+    },
+  ];
