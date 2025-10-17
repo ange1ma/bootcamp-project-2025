af877ae1a1a9772b8c01a9bc352a764f57c97399
ange1ma <siyinmaa@gmail.com>
2025-10-16 22:16:13 -0700

    feat: add more comments

--- src/blog.js
+++ src/blog.js
@@ -1,3 +1,4 @@
+// blog.ts converted into blog.js
 var blogs = [
     {
         title: "Iced Matcha Latte Recipe",
--- src/blog.ts
+++ src/blog.ts
@@ -1,14 +1,17 @@
+//define typescript type: "Blog"
 type Blog = {
     title: string;
     date: string;
     description: string;
-    image: string;
-    imageAlt: string;
-    slug: string;
+    image: string; //ex "./images/abc.png"
+    imageAlt: string; //describes an image
+    slug: string; //url name used to redirect to a specific page
 };
 
+//list of Blog objects
 const blogs: Blog[] = [
-    {
+    {   
+        //blog object 1
         title: "Iced Matcha Latte Recipe",
         date: "10-10-2025",
         description: "A quick recipe for an iced matcha latte ★",
@@ -16,7 +19,8 @@ const blogs: Blog[] = [
         imageAlt: "this is an image",
         slug: "iced-matcha-latte-recipe",
     },
-    {
+    {   
+        //blog object 2
         title: "Strawberry Milk Recipe",
         date: "10-16-2025",
         description: "planning costume ideas",
@@ -26,10 +30,16 @@ const blogs: Blog[] = [
     },
   ];
 
-  const blogContainer = document.getElementById('blog-container');
+//container for where new Blog elements will be inserted in
+//accessed via 'blog-container' in blog.html
+const blogContainer = document.getElementById('blog-container');
 
-  blogs.forEach((blog) => {
-    const image = document.createElement("img");
+//function to iterate over a list of blogs
+//.forEach is a built-in function that allows us to iterate over a list of blogs
+//it also allows us to access the attributes of blog
+blogs.forEach((blog) => {
+    //create blog elements
+    const image = document.createElement("img"); //specific code section from https://www.notion.so/h4i/Milestone-1-246197abf07b8121a166efd2a9d34fbf#246197abf07b812fbf33c1ffb0a1d35c
     image.src = blog.image;
     image.alt = blog.imageAlt;
 
@@ -43,8 +53,10 @@ const blogs: Blog[] = [
     description.textContent = blog.description;
 
     const post = document.createElement("div");
-    post.className = "post";
+    post.className = "post"; //created for styling purposes
     
+    //append elements into post
     post.append(image, title, date, description);
+    //append post to blogcontainer if exists
     blogContainer?.append(post);
-  })
+})
