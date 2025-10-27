b7c9431089f07b64c00d2799902ae97f0108bc9f
ange1ma <siyinmaa@gmail.com>
2025-10-27 02:45:46 -0700

    finished setting up next.js

---
+++ bootcamp-milestone-2/src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import localFont from "next/font/local";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Angel Ma's Personal Website",
+  description: "A personal website for Angel Ma.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
