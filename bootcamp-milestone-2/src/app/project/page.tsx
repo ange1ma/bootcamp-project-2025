9aaa84ec3f703d1f0eb0fdeb1c127e5364ff6d4e
ange1ma <siyinmaa@gmail.com>
2025-10-30 21:22:35 -0700

    fix link

---
+++ bootcamp-milestone-2/src/app/project/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Portfolio() {
+  return (
+    <main>
+      <h1 className="page-title">Portfolio</h1>
+      <div className="project">
+        <Link href="/">
+          <Image src="/website.png" alt="A screenshot of my website" width={450} height={400}/>
+          </Link>
+        <div className="project-details">
+          <p className="project-name">Angel's Personal Website</p>
+          <p className="project-description">
+            A project created with the help of H4I's starter pack, made with 
+            HTML and CSS. The picture features the bare bones of the website before CSS styling.
+          </p>
+          <Link href="/">Learn more!</Link>
+        </div>
+      </div>
+    </main>
+  );
+}
