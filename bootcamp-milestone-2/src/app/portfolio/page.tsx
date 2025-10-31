534f98c2325b292635bbc4ceb149eb6d49dab42c
ange1ma <siyinmaa@gmail.com>
2025-10-30 20:14:24 -0700

    copy portfolio.html onto portfolio/page.tsx and change html into tsx

---
+++ bootcamp-milestone-2/src/app/portfolio/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function Portfolio() {
+  return (
+    <main>
+      <h1 className="page-title">Portfolio</h1>
+      <div className="project">
+        <a href="/">
+          <Image src="/website.png" alt="A screenshot of my website" width={450} height={400}/>
+          </a>
+        <div className="project-details">
+          <p className="project-name">Angel's Personal Website</p>
+          <p className="project-description">
+            A project created with the help of H4I's starter pack, made with 
+            HTML and CSS. The picture features the bare bones of the website before CSS styling.
+          </p>
+          <a href="/">Learn more!</a>
+        </div>
+      </div>
+    </main>
+  );
+}
