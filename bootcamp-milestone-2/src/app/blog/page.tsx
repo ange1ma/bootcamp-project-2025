5454b3de332b1b5088cf607abe612d32acad5103
ange1ma <siyinmaa@gmail.com>
2025-10-30 18:02:01 -0700

    add page.tsx for blog

--- bootcamp-milestone-2/src/app/blog/page.tsx
+++ bootcamp-milestone-2/src/app/blog/page.tsx
@@ -1,4 +1,6 @@
 import Navbar from "@/components/navbar";
+import BlogPreview from "@/components/blogPreview";
+import blogs from "@/app/blogData";
 
 export default function Blog() {
   return (
@@ -6,6 +8,17 @@ export default function Blog() {
       <Navbar/>
       <h1 className="page-title">Blog</h1>
       <div id="blog-container">
+        {blogs.map((blog) => (
+          <BlogPreview
+            key={blog.title}
+            title={blog.title}
+            description={blog.description}
+            image={blog.image}
+            imageAlt={blog.imageAlt}
+            date={blog.date}
+            slug={blog.slug}
+          />
+        ))}
         <p>This is my blog!</p>
       </div>
       <footer className="footer">
