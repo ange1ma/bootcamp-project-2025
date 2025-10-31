0554ae4b3e9f3353dc47c4dc0f0b821ddc80b325
ange1ma <siyinmaa@gmail.com>
2025-10-30 19:50:06 -0700

    change blog.html to tsx foramt

---
+++ bootcamp-milestone-2/src/app/blogs/page.tsx
@@ -0,0 +1,24 @@
+import Navbar from "@/components/navbar";
+import BlogPreview from "@/components/blogPreview";
+import blogs from "@/app/blogData";
+
+export default function Blogs() {
+  return (
+    <main>
+      <h1 className="page-title">Blog</h1>
+      <div id="blog-container">
+        {blogs.map((blog) => (
+          <BlogPreview
+            key={blog.title}
+            title={blog.title}
+            description={blog.description}
+            image={blog.image}
+            imageAlt={blog.imageAlt}
+            date={blog.date}
+            slug={blog.slug}
+          />
+        ))}
+      </div>
+    </main>
+  )
+}
