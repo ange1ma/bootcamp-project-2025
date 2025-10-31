1c064bbc459e10de0215e0e22c9f4c341a4fa58a
ange1ma <siyinmaa@gmail.com>
2025-10-30 22:50:52 -0700

    adjust blogPreview and its css

--- bootcamp-milestone-2/src/components/blogPreview.tsx
+++ bootcamp-milestone-2/src/components/blogPreview.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import style from './blogPreview.module.css';
 import Image from "next/image";
+import Link from "next/link";
 import { Blog } from "../app/blogData";
 
 export default function BlogPreview(props: Blog) {
   return (
-    <div className={style.div}>
-      <h3>{props.title}</h3>
+    <div className={style.post}>
+      <Link href={props.slug}><h3>{props.title}</h3></Link>
       <div>
 				<Image src={props.image} alt={props.imageAlt} width={500} height={500} ></Image>
         <p>{props.description}</p>
